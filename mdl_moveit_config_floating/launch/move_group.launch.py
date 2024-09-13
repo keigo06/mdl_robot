7dc77946e56e2a197688385416a9900ba90bfb89
keigo06 <go.kbirdblue411@gmail.com>
2024-09-13 18:03:31 +0900

    add moveit_config_floating

---
+++ mdl_moveit_config_floating/launch/move_group.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_move_group_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("simple_5dof_robot", package_name="mdl_moveit_config_floating").to_moveit_configs()
+    return generate_move_group_launch(moveit_config)
