dee6a4518a35315678ed402d49f38b9e3914a14c
keigo06 <go.kbirdblue411@gmail.com>
2024-12-10 16:43:38 +0900

    change cubes from list to dic

--- mdl_cube/src/assembly.py
+++ mdl_cube/src/assembly.py
@@ -1,10 +1,83 @@
-# assembly.py
+from cube import Cube  # Cubeクラスをインポート
+
+import math
+
+
 class Assembly:
-    def __init__(self, cubes):
-        self.cubes = cubes  # List of Cube objects
+    def __init__(self, num_cubes=5):
+        # cubesは辞書形式で登録
+        self.num_cubes = num_cubes
+        self.cubes = {}
 
-    def add_cube(self, cube):
-        self.cubes.append(cube)
+    # def get_cube(self, cube_id):
+    #     """
+    #     指定されたIDのCubeオブジェクトを返す
+    #     :param cube_id: キューブのID
+    #     :return: Cubeオブジェクト
+    #     """
+    #     for cube in self.cubes:
+    #         if cube.cube_id == cube_id:
+    #             return cube
+    #     return None
 
-    def get_assembly_state(self):
-        return [cube.get_state() for cube in self.cubes]
+    # def add_cube(self, position, euler_angles):
+    #     """
+    #     新しいキューブをアセンブリに追加する
+    #     :param position: キューブの初期位置
+    #     :param euler_angles: キューブの初期姿勢
+    #     """
+    #     new_cube = Cube(len(self.cubes), position, euler_angles)
+    #     self.cubes.append(new_cube)
+
+    # def connect_cubes(self, cube1_id, cube2_id, face1, face2):
+    #     """
+    #     2つのキューブを指定された面で結合する
+    #     :param cube1_id: キューブ1のID
+    #     :param cube2_id: キューブ2のID
+    #     :param face1: キューブ1の結合面
+    #     :param face2: キューブ2の結合面
+    #     """
+    #     cube1 = self.get_cube(cube1_id)
+    #     cube2 = self.get_cube(cube2_id)
+
+    #     if cube1 and cube2:
+    #         cube1.connect(face1, cube2, face2)
+    #         cube2.connect(face2, cube1, face1)
+
+    # def get_assembly_state(self):
+    #     """
+    #     アセンブリ全体の状態を返す（各キューブの状態を取得）
+    #     :return: アセンブリ全体のキューブの状態
+    #     """
+    #     return [cube.get_state() for cube in self.cubes]
+
+    def create_line_assembly(self):
+        """ キューブを直線状に配置するアセンブリを作成 """
+        for id in range(self.num_cubes):
+            self.cubes[id] = Cube(id, [id * 0.12, 0.0, 0.06], [0.0, 0.0, 0.0])
+
+    def create_tower_assembly(self):
+        """ キューブをタワー状に配置するアセンブリを作成 """
+        for id in range(self.num_cubes):
+            self.cubes[id] = Cube(
+                id, [0.0, 0.0, 0.06 + id*0.12], [0.0, 0.0, 0.0])
+
+    def find_factors(n):
+        """Return a list of factors of n"""
+        factors = []
+        for i in range(1, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                factors.append((i, n // i))
+        return factors
+
+    def create_grid_assembly(self):
+        """ キューブをグリッド状に配置するアセンブリを作成 """
+        factors = self.find_factors(self.num_cubes)
+        grid_x, grid_y = min(factors, key=lambda f: abs(f[0] - f[1]))
+        for i in range(grid_y):
+            for j in range(grid_x):
+                self.cubes[i * grid_x + j] = Cube(
+                    cube_id=i * grid_x + j,
+                    position=[i * 0.12, j * 0.12, 0.06],
+                    euler_angles=[0.0, 0.0, 0.0]
+                )
--- mdl_cube/src/server.py
+++ mdl_cube/src/server.py
@@ -9,23 +9,23 @@ import math
 class Server(Node):
     def __init__(self):
         super().__init__('server')
-        self.cube_node = CubeNode(num_cubes=5)
+        self.cube_node = CubeNode(num_cubes=6)
         self.timer_period = 0.1
-        self.timer = self.create_timer(self.timer_period, self.update_cubes)
-        self.offsets = [i * 0.5 for i in range(5)]  # 各キューブのオフセット時間
+        self.timer = self.create_timer(self.timer_period, self.update_asm)
+        # self.offsets = [i * 0.5 for i in range(5)]  # 各キューブのオフセット時間
         self.start_seconds = self.get_clock().now().seconds_nanoseconds()[0]
         self.start_nanoseconds = self.get_clock(
         ).now().seconds_nanoseconds()[1]
         self.start_time = self.start_seconds + self.start_nanoseconds * 1e-9
 
-    def update_cubes(self):
+    def update_asm(self):
         """
-        定期的に呼ばれる。キューブの位置を更新し、手動でパブリッシュとブロードキャストを行う。
+        キューブの位置を更新し、手動でパブリッシュとブロードキャストを行う。
         """
-        current_seconds = self.get_clock().now().seconds_nanoseconds()[0]
-        current_nanoseconds = self.get_clock().now().seconds_nanoseconds()[1]
-        current_time = current_seconds + current_nanoseconds * 1e-9
-        pass_time = current_time - self.start_time
+        # current_seconds = self.get_clock().now().seconds_nanoseconds()[0]
+        # current_nanoseconds = self.get_clock().now().seconds_nanoseconds()[1]
+        # current_time = current_seconds + current_nanoseconds * 1e-9
+        # pass_time = current_time - self.start_time
 
         # 各キューブの位置を時間に基づいて更新
         # self.cube_demo_movement(pass_time)
@@ -33,14 +33,14 @@ class Server(Node):
         # キューブの状態を手動でパブリッシュ・ブロードキャスト
         self.cube_node.publish_and_broadcast()
 
-    def cube_demo_movement(self, pass_time):
-        """
-        デモ用の関数: 時間経過に応じてキューブの位置を更新する
-        """
-        for i, cube in enumerate(self.cube_node.cubes):
-            new_x = 2.0 + math.cos(pass_time + self.offsets[i])  # X方向にコサインで移動
-            new_y = 2.0 + math.sin(pass_time + self.offsets[i])  # Y方向にサインで移動
-            cube.update_position([new_x, new_y, cube.position[2]])  # Zは固定
+    # def cube_demo_movement(self, pass_time):
+    #     """
+    #     デモ用の関数: 時間経過に応じてキューブの位置を更新する
+    #     """
+    #     for i, cube in enumerate(self.cube_node.assembly):
+    #         new_x = 2.0 + math.cos(pass_time + self.offsets[i])  # X方向にコサインで移動
+    #         new_y = 2.0 + math.sin(pass_time + self.offsets[i])  # Y方向にサインで移動
+    #         cube.update_position([new_x, new_y, cube.position[2]])  # Zは固定
 
     def run(self):
         try:
